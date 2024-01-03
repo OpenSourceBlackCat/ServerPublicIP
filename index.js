e8f9d4cfde4455c86445be71e8a99a756c41cef6
OpenSourceBlackCat <amey992004@gmail.com>
2024-01-04 04:21:47 +0530

    Meow

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const {Client, GatewayIntentBits} = require("discord.js");
 // require("dotenv").config({path: ".env"});
 var request = require("request");
-
-_TOKEN = "";
-
-
-_MANAGER = 730518724984963183;
 _CODE = "```";
 const AmeyaBot = new Client({intents:[GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping]});
 
 const sendIP = async() =>{
-    var Manager = await AmeyaBot.users.fetch(_MANAGER);
+    var Manager = await AmeyaBot.users.fetch(process.env.MANAGER);
     request("http://ifconfig.me", async function(error, response, body) {
     if (!error && response.statusCode == 200) {
         await Manager.send(`Prajyot's Server Public IP: ${_CODE}${String(body)}${_CODE}`);
@@ -21,9 +16,9 @@ AmeyaBot.on("ready", async()=>{
     await sendIP();
 });
 AmeyaBot.on("messageCreate", async(ctx)=>{
-    if(!ctx.author.bot && ctx.author.id==_MANAGER){
+    if(!ctx.author.bot && ctx.author.id==process.env.MANAGER){
         await sendIP();
     }
 })
-AmeyaBot.login(_TOKEN);
+AmeyaBot.login(process.env.TOKEN);
 
